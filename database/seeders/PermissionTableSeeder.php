f22fda061864b34232dde1950f77bc25bea75e0b
Pobitrodeb <pobitrodn@gmail.com>
2023-02-13 23:05:19 +0600

    database seeder added

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Permission;
 class PermissionTableSeeder extends Seeder
 {
     /**
@@ -14,6 +14,20 @@ class PermissionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Permissions
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
     }
 }
