2103cbf0de638259acbb80c61a37802fe79c0992
Pobitrodeb <pobitrodn@gmail.com>
2023-02-13 21:11:05 +0600

    construct declar success

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,11 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+
+  function __construct()
+  {
+    $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index','show']]);
+    $this->middleware('permission:product-create', ['only' => ['create','store']]);
+    $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
+    $this->middleware('permission:product-delete', ['only' => ['distroy']]);
+
+  }
+
     /**
      * Display a listing of the resource.
      *
