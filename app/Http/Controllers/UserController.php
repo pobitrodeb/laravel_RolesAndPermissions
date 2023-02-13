bea23aec67036e486c3d3d47ec5598a09c07797f
Pobitrodeb <pobitrodn@gmail.com>
2023-02-13 20:07:13 +0600

    show users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,7 +63,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        return view('users.show',compact('user'));
     }
 
     /**
